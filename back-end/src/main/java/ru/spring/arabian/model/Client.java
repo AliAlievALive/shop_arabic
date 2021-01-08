f1427100d9797bc32b3cea433be5d17ebad124d0
Almir Almiev <madarat@mail.ru>
2021-01-08 15:31:08 +0300

    add annotations (Client and Product) #2

--- back-end/src/main/java/ru/spring/arabian/model/Client.java
+++ back-end/src/main/java/ru/spring/arabian/model/Client.java
@@ -1,17 +1,38 @@
 package ru.spring.arabian.model;
 
+import org.hibernate.annotations.CreationTimestamp;
 import ru.spring.arabian.model.enums.Gender;
 
-import javax.xml.crypto.Data;
+import javax.persistence.*;
+import java.util.Date;
 
+@Entity(name = "client")
 public class Client {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "fullname")
     private String fullName;
+
+    @Column(name = "gender")
     private Gender gender;
+
+    @Column(name = "phone-number")
     private String phone;
+
+    @Column(name = "age")
     private String age;
+
+    @Column(name = "address")
     private String address;
-    private Data dataCreated;
-    private Data lastIn;
+
+    @CreationTimestamp
+    @Column(name = "created_date")
+    private Date dateCreated;
+
+    @Column(name = "visit_date")
+    private Date dateLastVisit;
 
 }
--- back-end/src/main/java/ru/spring/arabian/model/Product.java
+++ back-end/src/main/java/ru/spring/arabian/model/Product.java
@@ -1,10 +1,24 @@
 package ru.spring.arabian.model;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
 import java.util.Date;
 
+@Entity(name = "product")
 public class Product {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
     private String Name;
+
+    @Column(name = "price")
     private int price;
+
+    @CreationTimestamp
+    @Column(name = "created-date")
     private Date dataCreated;
 }
