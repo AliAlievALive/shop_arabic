a602b18467ff12c85693e6977bd72c953a77a529
Almir Almiev <madarat@mail.ru>
2021-01-09 20:23:32 +0300

    Handle routes and router-link Closes #19

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,21 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HeaderComponent } from './components/header/header.component';
-import { RegisterComponent } from './components/register/register.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { ProductsComponent } from './components/products/products.component';
-import { ClientOptionsComponent } from './components/client-options/client-options.component';
-import { ProductOptionsComponent } from './components/product-options/product-options.component';
+import {AppComponent} from './app.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {RegisterComponent} from './components/register/register.component';
+import {ClientsComponent} from './components/clients/clients.component';
+import {ProductsComponent} from './components/products/products.component';
+import {ClientOptionsComponent} from './components/client-options/client-options.component';
+import {ProductOptionsComponent} from './components/product-options/product-options.component';
 import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
-  {path: 'register', component: RegisterComponent },
-  {path: 'control', component: RegisterComponent },
-  {path: 'products', component: RegisterComponent }
+  {path: 'register', component: RegisterComponent},
+  {path: 'product_options', component: ProductOptionsComponent},
+  {path: 'client_options', component: ClientOptionsComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'clients', component: ClientsComponent},
+  {path: '**', component: ProductsComponent}
 ];
+
 @NgModule({
   declarations: [
     AppComponent,
