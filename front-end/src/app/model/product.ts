e38b31a521756cb31aa4bc253bd7de44832eceb5
Almir Almiev <madarat@mail.ru>
2021-01-09 17:34:40 +0300

    add stock attribute in Product Closes #16

--- front-end/src/app/model/product.ts
+++ front-end/src/app/model/product.ts
@@ -2,12 +2,14 @@ export class Product {
   id: number;
   name: string;
   price: number;
+  stock: number;
   created: Date;
 
-  constructor(id: number, name: string, price: number, created: Date) {
+  constructor(id: number, name: string, price: number, stock: number, created: Date) {
     this.id = id;
     this.name = name;
     this.price = price;
+    this.stock = stock;
     this.created = created;
   }
 }
