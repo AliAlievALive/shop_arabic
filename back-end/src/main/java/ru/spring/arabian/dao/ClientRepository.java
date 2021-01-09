3191be43b4db50f4eb0a7b68be2f9a770dad3e4e
Almir Almiev <madarat@mail.ru>
2021-01-09 23:22:05 +0300

    add ids to entities Closes #22

--- back-end/src/main/java/ru/spring/arabian/dao/ClientRepository.java
+++ back-end/src/main/java/ru/spring/arabian/dao/ClientRepository.java
@@ -2,8 +2,10 @@ package ru.spring.arabian.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.spring.arabian.model.Client;
 
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
