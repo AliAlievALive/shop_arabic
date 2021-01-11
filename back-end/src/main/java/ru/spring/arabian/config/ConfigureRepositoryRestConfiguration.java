41afa46bca5e47f738dd7b65788cef23eebdff2a
Almir Almiev <madarat@mail.ru>
2021-01-11 15:33:26 +0300

    disabled method (GET, POST, PUT, DELETE) Closes #37

--- back-end/src/main/java/ru/spring/arabian/config/ConfigureRepositoryRestConfiguration.java
+++ back-end/src/main/java/ru/spring/arabian/config/ConfigureRepositoryRestConfiguration.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import ru.spring.arabian.model.Client;
+import ru.spring.arabian.model.Product;
 
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
@@ -24,7 +27,17 @@ public class ConfigureRepositoryRestConfiguration implements RepositoryRestConfi
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-        this.exposeIds(config);
+        HttpMethod[] unsupported = {HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
+        disableHttpMethod(Client.class, config, unsupported);
+        disableHttpMethod(Product.class, config, unsupported);
+        exposeIds(config);
+    }
+
+    public void disableHttpMethod(Class thisClass, RepositoryRestConfiguration config, HttpMethod[] unsupportedMethod) {
+        config.getExposureConfiguration()
+                .forDomainType(thisClass)
+                .withItemExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod)))
+                .withCollectionExposure(((metdata, httpMethods) -> httpMethods.disable(unsupportedMethod)));
     }
 
     public void exposeIds(RepositoryRestConfiguration config) {
