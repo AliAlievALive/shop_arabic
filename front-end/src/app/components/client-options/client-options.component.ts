de4eba3fd4d09e0bf441c7745bd859d9b00f196f
Almir Almiev <madarat@mail.ru>
2021-01-12 16:26:55 +0300

    get data form Closes #42

--- front-end/src/app/components/client-options/client-options.component.ts
+++ front-end/src/app/components/client-options/client-options.component.ts
@@ -15,7 +15,7 @@ export class ClientOptionsComponent implements OnInit {
   ngOnInit(): void {
     this.clientGroup= this.formBuilder.group({
       client: this.formBuilder.group({
-        userName: ['Имя'],
+        name: ['Имя'],
         age: ['Возраст'],
         address: ['Адрес'],
         phone: ['Телефонный номер'],
@@ -26,7 +26,31 @@ export class ClientOptionsComponent implements OnInit {
     })
   }
 
-  add() {
+  getName() {
+    return this.clientGroup.get('client').value.name;
+  }
 
+  getAge() {
+    return this.clientGroup.get('client').value.age;
+  }
+
+  getAddress() {
+    return this.clientGroup.get('client').value.address;
+  }
+
+  getPhone() {
+    return this.clientGroup.get('client').value.phone;
+  }
+
+  getGender() {
+    return this.clientGroup.get('client').value.gender;
+  }
+
+  add() {
+    console.log(this.getName());
+    console.log(this.getAge());
+    console.log(this.getAddress());
+    console.log(this.getPhone());
+    console.log(this.getGender());
   }
 }
